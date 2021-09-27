157d5049d82a1ffaab1a99b5beb2e34f0a3c4670
y3km21 <y3km21@gmail.com>
2021-09-27 22:36:54 +0900

    formattiong, mount

--- nix_install.sh
+++ nix_install.sh
@@ -11,4 +11,22 @@ sudo parted /dev/sda -- mkpart primary linux-swap -4GiB 100%
 sudo parted /dev/sda -- mkpart ESP fat32 1MiB 512MiB
 sudo parted /dev/sda -- set 3 esp on
 
+#Formatting
+# root
+sudo mkfs.ext4 -L nixos /dev/sda1
+# swap
+sudo mkswap -L swap /dev/sda2
+# boot
+sudo mkfs.fat -F 32 -n boot /dev/sda3
+
+
+#Mount
+# root
+sudo mount /dev/disk/by-label/nixos /mnt
+# boot
+sudo mkdir -p /mnt/boot
+sudo mount /dev/disk/by-label/boot /mnt/boot
+# swap
+sudo swapon /dev/sda2
+
 
