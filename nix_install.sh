5f17c70f8cc36d43566eab23c1590b843e7ac55c
y3km21 <y3km21@gmail.com>
2021-09-28 12:53:17 +0900

    bios/gpt

--- nix_install.sh
+++ nix_install.sh
@@ -1,15 +1,17 @@
 #!/run/current-system/sw/bin/bash
 
-#Parted
+# --Parted
+# BIOS/GPT
 # make gpt partition table
 sudo parted /dev/sda -- mklabel gpt
 # add root partition
-sudo parted /dev/sda -- mkpart primary 512MiB -4GiB
+sudo parted /dev/sda -- mkpart primary 100MiB -4GiB
 # add swap partition
 sudo parted /dev/sda -- mkpart primary linux-swap -4GiB 100%
 # add boot partition
-sudo parted /dev/sda -- mkpart ESP fat32 1MiB 512MiB
-sudo parted /dev/sda -- set 3 esp on
+sudo parted /dev/sda -- mkpart primary 1MiB 100MiB
+sudo parted /dev/sda -- set 3 boot on
+sudo parted /dev/sda -- set 3 bios_grub on
 
 #Formatting
 # root
@@ -17,7 +19,7 @@ sudo mkfs.ext4 -L nixos /dev/sda1
 # swap
 sudo mkswap -L swap /dev/sda2
 # boot
-sudo mkfs.fat -F 32 -n boot /dev/sda3
+sudo mkfs.ext4 -L boot /dev/sda3
 
 
 #Mount
